3c1e5191f6898179c6e5f9dca640de7bc228fcde
electricas <mtk.electricas@gmail.com>
2020-11-02 16:58:41 +0100

    In testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_performance/firebase_performance.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+FirebaseAnalytics analytics;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  analytics = FirebaseAnalytics();
+
+  FirebaseCrashlytics.instance.enableInDevMode = false;
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FirebaseCrashlytics.instance.recordFlutterError(details);
+  };
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'SocGo!'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +48,130 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  _analyticsEvent() {
+    analytics.logEvent(name: 'button_press');
+    print('event logged');
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _perfTrace() async {
+    Trace trace = FirebasePerformance.instance.newTrace('trace_test');
+    trace.start();
+    await Future.delayed(const Duration(seconds: 5));
+  }
+
+  _crash() {
+    throw FlutterError('This is a test');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.white, systemNavigationBarColor: Colors.white));
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              widget.title,
+              style: TextStyle(color: Colors.black),
+            ),
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            elevation: 1,
+            leadingWidth: 65,
+            leading: IconButton(
+              icon: Icon(Icons.chat_bubble_outline),
+              onPressed: () {},
+              color: Colors.grey.shade600,
+            ),
+            actions: [
+              IconButton(
+                  icon: CircleAvatar(backgroundColor: Colors.red, radius: 17.0),
+                  constraints: BoxConstraints(minWidth: 65),
+                  onPressed: () {}),
+            ],
+            snap: true,
+            floating: true,
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () => {
+                    analytics.logEvent(name: 'button_press'),
+                    print('logged analytics event'),
+                  },
+                  color: Color(0xFF3B84F0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  textColor: Colors.white,
+                  elevation: 0.0,
+                  child: const Text('Log Analytics Event'),
+                ),
+                RaisedButton(
+                  onPressed: () => {_perfTrace, print('logged perf event')},
+                  color: Color(0xFF3B84F0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  textColor: Colors.white,
+                  elevation: 0.0,
+                  child: const Text('Log Performance Event'),
+                ),
+                RaisedButton(
+                  onPressed: () => {
+                    //FirebaseCrashlytics.instance.crash(),
+                    throw Exception("Test exception"),
+                  },
+                  color: Color(0xFF3B84F0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  textColor: Colors.white,
+                  elevation: 0.0,
+                  child: const Text('Throw error/Crash'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      /*body: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            RaisedButton(
+              onPressed: () => {
+                analytics.logEvent(name: 'button_press'),
+                print('logged analytics event'),
+              },
+              color: Color(0xFF3B84F0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              textColor: Colors.white,
+              elevation: 0.0,
+              child: const Text('Log Analytics Event'),
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            RaisedButton(
+              onPressed: () => {_perfTrace, print('logged perf event')},
+              color: Color(0xFF3B84F0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              textColor: Colors.white,
+              elevation: 0.0,
+              child: const Text('Log Performance Event'),
+            ),
+            RaisedButton(
+              onPressed: () => {_crash, print('thrown error')},
+              color: Color(0xFF3B84F0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              textColor: Colors.white,
+              elevation: 0.0,
+              child: const Text('Throw error/Crash'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),*/
     );
   }
 }
